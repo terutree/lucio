bc7d0188f8f12160c05d16d1bfcb42b5de1a0f9a
Stiegler <trygve.stiegler@capgemini.com>
2022-12-07 16:56:56 +0000

    murf

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRunTimeCompilation;
 
 var app = builder.Build();
 
